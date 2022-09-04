45405d5b366ecf7bc638878a34e0e4eab03f6a32
Kseniya <pashkina.ks@mail.ru>
2022-09-04 20:09:54 +0700

    Задача4

---
+++ Задача_4/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Введите первое число");
+
+string number_1 = Console.ReadLine();
+
+int a = int.Parse(number_1);
+
+Console.WriteLine("Введите второе число");
+
+string number_2= Console.ReadLine();
+
+int b = int.Parse(number_2);
+
+Console.WriteLine("Введите третье число");
+
+string number_3 = Console.ReadLine();
+
+int c = int.Parse(number_3);
+
+if (a > b && b > c) {
+    Console.WriteLine($"Максимальное число: {a}");
+}
+else if (b > a && a > c) {
+    Console.WriteLine($"Максимальное число: {b}");
+}
+else
+{
+    Console.WriteLine($"Максимальное число: {c}");
+}
