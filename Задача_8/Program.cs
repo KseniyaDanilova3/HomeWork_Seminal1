4bd9938c5e31593fbbc0267c73eb88c2782131ad
Kseniya <pashkina.ks@mail.ru>
2022-09-04 21:22:09 +0700

    Задача8

---
+++ Задача_8/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите число: ");
+
+string number = Console.ReadLine();
+
+int n = int.Parse(number);
+if (n < 2) {
+    Console.Write($" Укажите число больше 1 ");
+}
+
+for ( int i = 2 ; i <= n; i = i + 1) {
+    if (i % 2 == 0) {
+        Console.Write($" {i} ");
+    }
+}
