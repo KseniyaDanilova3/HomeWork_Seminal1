218bd8c3036be7df244a44fa83e2f17a95f00de7
Kseniya <pashkina.ks@mail.ru>
2022-09-04 20:18:32 +0700

    Задача6

---
+++ Задача_6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите любое число:");
+
+string number = Console.ReadLine();
+
+int numberA = int.Parse(number);
+
+if (numberA % 2 == 0 ) {
+    Console.WriteLine($"{numberA} - четное число!");
+}
+else {
+   Console.WriteLine($"{numberA} - нечетное число!");
+}
